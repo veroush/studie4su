857e6369e43465f5b47df499c9419a28dacb4bab
skk25 <s.kempes@unasat.sr>
2026-08-11 13:45:07 -0300

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Example Guide', slug: 'guides/how-to-use' },
 					],
 				},
 				{
